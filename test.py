0e42246adc736c80094c5fd6e1f96b8d12666361
TManninger-smg <tanja.arbeit.smg@gmail.com>
2025-04-14 11:49:42 +0000

    delete stuff

--- test.py
+++
@@ -1,9 +0,0 @@
-import chardet
-
-file_path = "Data_Categories.CSV"
-
-with open(file_path, "rb") as f:
-    rawdata = f.read()
-
-result = chardet.detect(rawdata)
-print(result)  # This will show the detected encoding
